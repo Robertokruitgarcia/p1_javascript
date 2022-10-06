e060409487699be3cdbcde7a3ffb986800df44b4
Burger <9020693@student.zadkine.nl>
2022-10-06 14:53:04 +0200

    Added 6.4

---
+++ opdracht-6.4/app.js
@@ -0,0 +1,20 @@
+function calculate(number = 2){
+    number -= 1;
+    console.log("calculate output: " + number);
+}
+
+calculate();
+
+function multiplyByFive(number2 = 3){
+    number2 *= 5;
+    console.log("multiplyByFive output: " + number2)
+}
+
+multiplyByFive();
+
+function calculateMinutes(Minutes = 3600){
+    Minutes /= 60;
+    console.log("calculateMinutes output: " + Minutes)
+}
+
+calculateMinutes();

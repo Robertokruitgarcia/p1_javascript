cfb35716b6547b137f71009040635cdfc4f30d62
Burger <9020693@student.zadkine.nl>
2022-11-01 12:25:44 +0100

    Opdracht 8.1

--- opdracht-8.1/app.js
+++ opdracht-8.1/app.js
@@ -2,26 +2,42 @@ function multiply(){
     let Input1 = document.getElementById("Input_1").value;
     let Input2 = document.getElementById("Input_2").value;
 
-    document.getElementById("H1").innerHTML = Input1 * Input2;
+    if(Input1 || Input2 <= 0){
+        document.getElementById("H1").innerText = "het getal is te laag";
+    }else{
+        document.getElementById("H1").innerText = Input1 * Input2;
+    }
 }
 
 function devide(){
     let Input1 = document.getElementById("Input_1").value;
     let Input2 = document.getElementById("Input_2").value;
 
-    document.getElementById("H1").innerHTML = Input1 / Input2;
+    if(Input1 || Input2 <= 0){
+        document.getElementById("H1").innerText = "het getal is te laag";
+    }else{
+        document.getElementById("H1").innerText = Input1 * Input2;
+    }
 }
 
 function add(){
     let Input1 = document.getElementById("Input_1").value;
     let Input2 = document.getElementById("Input_2").value;
 
-    document.getElementById("H1").innerHTML = +Input1 + + Input2;
+    if(Input1 || Input2 <= 0){
+        document.getElementById("H1").innerText = "het getal is te laag";
+    }else{
+        document.getElementById("H1").innerText = Input1 * Input2;
+    }
 }
 
 function minus(){
     let Input1 = document.getElementById("Input_1").value;
     let Input2 = document.getElementById("Input_2").value;
 
-    document.getElementById("H1").innerHTML = Input1 - Input2;
+    if(Input1 || Input2 <= 0){
+        document.getElementById("H1").innerText = "het getal is te laag";
+    }else{
+        document.getElementById("H1").innerText = Input1 * Input2;
+    }
 }
